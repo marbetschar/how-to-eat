2836ba233d77a69251c9358faf52af6db87b71fc
David Kalchofner <david.kalchofner@gmail.com>
2019-11-17 09:53:40 +0100

    update ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     groupedSumByFoodCategory,
     dailyMinimalCaloriesFor,
@@ -7,7 +7,7 @@ import {
 } from './functions';
 import './App.css';
 
-import {Button, Select, MenuItem, TextField, Table, TableBody, TableHead, TableRow, TableCell} from "@material-ui/core";
+import { Button, Select, MenuItem, TextField, Table, TableBody, TableHead, TableRow, TableCell } from "@material-ui/core";
 
 function App() {
 
@@ -62,20 +62,20 @@ function App() {
         var foodNames = foodList.map((foodItem) => { return foodItem.name });
         console.log('https://how-to-eat.eu-gb.cf.appdomain.cloud/names/' + foodNames.join(","));
         fetch('https://how-to-eat.eu-gb.cf.appdomain.cloud/names/' + foodNames.join(","))
-        .then(apiResponse => {
-            console.log('foodList:', foodList);
-            var groupedSum = groupedSumByFoodCategory(foodList, apiResponse);
-            var dailyCalories = dailyMinimalCaloriesFor({ adults: adultCount, children: childCount });
-            
-            var menuForDay = [];
-            while( groupedSumByFoodCategoryContainsFood(groupedSum) ){
-                menuForDay.push(planFoodForOneDay(groupedSum, dailyCalories));
-            }
-            console.log(menuForDay);
-        })
-        .catch((error) => {
-            console.error(error);
-        })
+            .then(apiResponse => {
+                console.log('foodList:', foodList);
+                var groupedSum = groupedSumByFoodCategory(foodList, apiResponse);
+                var dailyCalories = dailyMinimalCaloriesFor({ adults: adultCount, children: childCount });
+
+                var menuForDay = [];
+                while (groupedSumByFoodCategoryContainsFood(groupedSum)) {
+                    menuForDay.push(planFoodForOneDay(groupedSum, dailyCalories));
+                }
+                console.log(menuForDay);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
     };
 
     const removeItemFromList = (index) => {
@@ -85,59 +85,69 @@ function App() {
         console.log('list', foodList);
     };
 
+
     return (
-    <div className="App">
-        <div className="header"><h1>How to eat</h1></div>
-        <div className="people">
-            <div className="addPeopleButtonsContainer">
-            <Button onClick={addAdult}>Add an adult</Button>
-            <Button onClick={addChild}>Add a child</Button>
-            </div>
-            <div className="listPeople">
-            <p>Adults: {adultCount} <Button onClick={removeAdult}>-</Button></p><br/>
-            <p>Children: {childCount} <Button onClick={removeChild}>-</Button></p>
-            </div>
-        </div>
-        <div className="addFood">
-            <Button onClick={addFood}>Add food</Button>
-            <p>Food count in list: {foodCount}</p>
-            {foodToAdd &&
-                <div className="addFoodForm">
-                    <TextField id="foodName" placeholder="food name" />
-                    <TextField id="quantity" type="number" placeholder="quantity" />
-                    <Select id="unit">
-                        <MenuItem value="gr">grams</MenuItem>
-                        <MenuItem value="piece">pieces</MenuItem>
-                    </Select>
-                    <Button onClick={handleSubmit}>Add</Button>
+        <div className="App col-md-12">
+            <div className="header"><h1>How to eat</h1></div>
+            <div class="row">
+                <div className="people col-md-4">
+                    <div className="listPeople">
+                        <div class="adult"><p>How many adults?</p>
+                            <Button onClick={addAdult}>+</Button>
+                            <p>{adultCount}</p>
+                            <Button onClick={removeAdult}>-</Button>
+                        </div>
+                        <div class="child">
+                            <p>How man children?</p>
+                            <Button onClick={addChild}>+</Button>
+                            <p>{childCount}</p>
+                            <Button onClick={removeChild}>-</Button>
+                        </div>
+                    </div>
                 </div>
-            }
+                <div class="col-md-8">
+                    <div className="addFood">
+                        <div className="listFood">
+                            <p>Food count in list: {foodCount}</p>
+                            {foodList.length > 0 &&
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell><b>Food name</b></TableCell>
+                                            <TableCell><b>Quantity</b></TableCell>
+                                            <TableCell><b>Unit</b></TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {foodList.map(foodItem =>
+                                            <TableRow>
+                                                <TableCell>{foodItem.name}</TableCell>
+                                                <TableCell>{foodItem.quantity}</TableCell>
+                                                <TableCell>{foodItem.unit}</TableCell>
+                                            </TableRow>
+                                        )}
+                                    </TableBody>
+                                </Table>
+                            }
+                            <div className="addFoodForm">
+                                <TextField id="foodName" placeholder="food name" />
+                                <TextField id="quantity" type="number" placeholder="quantity" />
+                                <Select id="unit">
+                                    <MenuItem value="gr">grams</MenuItem>
+                                    <MenuItem value="piece">pieces</MenuItem>
+                                </Select>
+                            </div>
+                            <Button onClick={handleSubmit}>Add</Button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="result">
+                <Button onClick={handleCalculate}>Calculate</Button>
+            </div>
         </div>
-        <div className="listFood">
-            {foodList.length > 0 &&
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell><b>Food name</b></TableCell>
-                            <TableCell><b>Quantity</b></TableCell>
-                            <TableCell><b>Unit</b></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {foodList.map(foodItem =>
-                            <TableRow>
-                                <TableCell>{foodItem.name}</TableCell>
-                                <TableCell>{foodItem.quantity}</TableCell>
-                                <TableCell>{foodItem.unit}</TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            }
-        </div>
-        <Button onClick={handleCalculate}>Calculate</Button>
-    </div>
-  );
+    );
 }
 
 export default App;
